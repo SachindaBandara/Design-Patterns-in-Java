6c0ec0616c5970a3c2468d4619f792bdbb2e0cb5
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-07-04 21:39:29 +0530

    add

---
+++ Factory Design Pattern/Shape.java
@@ -0,0 +1,47 @@
+interface Shape {
+    void draw();
+}
+
+class Rectangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
+
+class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
+
+class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
+
+class ShapeFactory {
+
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+
+        return null;
+    }
+}
